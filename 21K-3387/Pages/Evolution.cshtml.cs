b66b9cc87a85f2aa7380b0cf79c1aebdd7c67599
hasanmehdavi <hasan.mehdavi@talverse.com>
2024-09-28 20:00:23 +0500

    Initial push

---
+++ 21K-3387/Pages/Evolution.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _21K_3387.Pages
+{
+    public class EvolutionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
